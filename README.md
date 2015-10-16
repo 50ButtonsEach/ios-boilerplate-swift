746cc5616249c2bdcd89cf0f7ab2e7ae35f609c5
AntonMeier <anton.meier91@gmail.com>
2015-10-16 14:26:37 +0200

    Commit of an empty Xcode project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ios-boilerplate-swift
+This project demonstrates how you can implement the fliclib framework in a Swift application. Please not that this project is just a basic setup and is therefore not set up for background execution or state preservation.
+
---
+++ ios-boilerplate-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-boilerplate-swift
+//
+//  Created by Anton Meier on 16/10/15.
+//  Copyright © 2015 Anton Meier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
